e0e63055c5fc5d170023cd2798af46b5dee20152
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-25 16:54:52 +0700

    reformat code

--- app/src/main/java/com/example/challengechapterthree/ui/FirstFragment.kt
+++ app/src/main/java/com/example/challengechapterthree/ui/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.example.challengechapterthree.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.challengechapterthree.R
 import com.example.challengechapterthree.databinding.FragmentFirstBinding
